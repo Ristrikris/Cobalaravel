d5686a64baf8f57c7cf18cf46c590efd98ba13e4
LAPTOP-6E6G0R3F\ASUS <ristri.krisnugraheni@si.ukdw.ac.id>
2021-02-20 10:44:52 +0700

    ubah nama

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    HALLLO
                 </div>
 
                 <div class="links">
